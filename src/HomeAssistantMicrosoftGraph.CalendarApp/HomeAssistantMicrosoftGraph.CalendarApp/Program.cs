cd0f83b705c55fbade992140b576621fece56ca7
Alex Oswald <alex.oswald@outlook.com>
2024-12-14 22:24:59 -0800

    Init

---
+++ src/HomeAssistantMicrosoftGraph.CalendarApp/HomeAssistantMicrosoftGraph.CalendarApp/Program.cs
@@ -0,0 +1,32 @@
+using HomeAssistantMicrosoftGraph.CalendarApp.Client.Pages;
+using HomeAssistantMicrosoftGraph.CalendarApp.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+}
+
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(HomeAssistantMicrosoftGraph.CalendarApp.Client._Imports).Assembly);
+
+app.Run();
